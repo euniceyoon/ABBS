46109988dbd3c2f1824bab72f614455281f1e3d8
euniceyoon <159868862+euniceyoon@users.noreply.github.com>
2025-04-21 23:37:44 -0700

    initial commit

---
+++ abbs edit2/abbs_edit2App.swift
@@ -0,0 +1,17 @@
+//
+//  abbs_edit2App.swift
+//  abbs edit2
+//
+//  Created by Eunice Yoon on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct abbs_edit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
